fea0a46585e6f20d563cb07f11ab5398dcda1b29
Visual-Rock <visualrock.5329@gmail.com>
2022-06-30 15:51:32 +0200

    feat: LogRushLogger + Log4Net Appender

---
+++ LogRushClient/Core/LogRushLogger.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace LogRushClient.Core
+{
+    class LogRushLogger
+    {
+        private struct RegisterLogRushStreamRequest
+        {
+            public RegisterLogRushStreamRequest() { }
+
+            public string alias { get; init; } = "";
+            public string id { get; init; } = "";
+            public string key { get; init; } = "";
+        }
+    
+        private struct UnregisterLogRushStreamRequest
+        {
+            public UnregisterLogRushStreamRequest() { }
+            public string id { get; init; } = "";
+            public string key { get; init; } = "";
+        }
+        
+        private struct LogMessageRequest
+        {
+            public LogMessageRequest() { }
+
+            public string stream { get; init; } = "";
+            public string log { get; init; } = "";
+            public long timestamp { get; init; } = 0;
+        }
+        
+        public LogRushLogger(string alias, string address, string id = "", string key = "")
+        {
+            using var client = new HttpClient();
+            
+            var json = new RegisterLogRushStreamRequest { alias = alias, id = id, key = key };
+
+            var httpRes = client.PostAsync($"{address}/stream/register", new StringContent(JsonSerializer.Serialize(json), Encoding.UTF8, "application/json"));
+            httpRes.Wait();
+            
+            var str = httpRes.Result.Content.ReadAsStringAsync();
+            str.Wait();
+            var response = JsonNode.Parse(str.Result);
+
+            if ( response!["message"] != null && response["message"]?.ToString() != "")
+                throw new InvalidOperationException(response!["message"]?.ToString());
+            
+            this._address = address;
+            this._id = response["id"]!.ToString();
+            this._key = response["key"]!.ToString();
+        }
+        
+        public void Deconstruct()
+        {
+            var json = new UnregisterLogRushStreamRequest { id = _id, key = _key};
+            
+            using var client = new HttpClient();
+            var httpRes = client.PostAsync($"{_address}/stream/unregister", new StringContent(JsonSerializer.Serialize(json), Encoding.UTF8, "application/json"));
+            httpRes.Wait();
+        }
+        
+        public void Log(string log)
+        {
+            using var client = new HttpClient();
+
+            var message = new LogMessageRequest
+            {
+                log = log,
+                timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                stream = _id
+            };
+
+            var res = client.PostAsync($"{_address}/log", new StringContent(JsonSerializer.Serialize(message), Encoding.UTF8, "application/json"));
+            res.Wait();
+            var str = res.Result.Content.ReadAsStringAsync();
+            str.Wait();
+            
+            var response = JsonNode.Parse(str.Result);
+
+            if ( response!["message"] != null && response["message"]?.ToString() != "")
+                throw new InvalidOperationException(response["message"]?.ToString());
+        }
+
+        private readonly string _address;
+        private readonly string _id;
+        private readonly string _key;
+    }
+}
---
+++ LogRushClient/Log4Net/LogRushLogger.cs
@@ -0,0 +1,21 @@
+using log4net.Appender;
+using log4net.Core;
+
+namespace LogRushClient.Log4Net
+{
+    public class LogRushLogger : AppenderSkeleton
+    {
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            _logger ??= new Core.LogRushLogger(Alias, Server, Id, Key);
+            _logger.Log(RenderLoggingEvent(loggingEvent));
+        }
+        
+        public string Server { set; get; } = "";
+        public string Alias { set; get; } = "";
+        public string Id { set; get; } = "";
+        public string Key { set; get; } = "";
+        
+        private Core.LogRushLogger? _logger;
+    }
+}
