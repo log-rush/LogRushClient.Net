b29ddd5e64fddec58a70725fd3453224e6750115
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2023-02-03 18:54:40 +0100

    .net 7.0 and async stuff

--- LogRushClient/Core/LogRushLogger.cs
+++ LogRushClient/Core/LogRushLogger.cs
@@ -2,89 +2,86 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
-namespace LogRushClient.Core
+namespace LogRushClient.Core;
+
+public class LogRushLogger
 {
-    class LogRushLogger
+    private struct RegisterLogRushStreamRequest
     {
-        private struct RegisterLogRushStreamRequest
-        {
-            public RegisterLogRushStreamRequest() { }
+        public RegisterLogRushStreamRequest() { }
 
-            public string alias { get; init; } = "";
-            public string id { get; init; } = "";
-            public string key { get; init; } = "";
-        }
-    
-        private struct UnregisterLogRushStreamRequest
-        {
-            public UnregisterLogRushStreamRequest() { }
-            public string id { get; init; } = "";
-            public string key { get; init; } = "";
-        }
-        
-        private struct LogMessageRequest
-        {
-            public LogMessageRequest() { }
-
-            public string stream { get; init; } = "";
-            public string log { get; init; } = "";
-            public long timestamp { get; init; } = 0;
-        }
-        
-        public LogRushLogger(string alias, string address, string id = "", string key = "")
-        {
-            using var client = new HttpClient();
-            
-            var json = new RegisterLogRushStreamRequest { alias = alias, id = id, key = key };
-
-            var httpRes = client.PostAsync($"{address}/stream/register", new StringContent(JsonSerializer.Serialize(json), Encoding.UTF8, "application/json"));
-            httpRes.Wait();
-            
-            var str = httpRes.Result.Content.ReadAsStringAsync();
-            str.Wait();
-            var response = JsonNode.Parse(str.Result);
-
-            if ( response!["message"] != null && response["message"]?.ToString() != "")
-                throw new InvalidOperationException(response!["message"]?.ToString());
-            
-            this._address = address;
-            this._id = response["id"]!.ToString();
-            this._key = response["key"]!.ToString();
-        }
-        
-        public void Deconstruct()
-        {
-            var json = new UnregisterLogRushStreamRequest { id = _id, key = _key};
-            
-            using var client = new HttpClient();
-            var httpRes = client.PostAsync($"{_address}/stream/unregister", new StringContent(JsonSerializer.Serialize(json), Encoding.UTF8, "application/json"));
-            httpRes.Wait();
-        }
-        
-        public void Log(string log)
-        {
-            using var client = new HttpClient();
-
-            var message = new LogMessageRequest
-            {
-                log = log,
-                timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
-                stream = _id
-            };
-
-            var res = client.PostAsync($"{_address}/log", new StringContent(JsonSerializer.Serialize(message), Encoding.UTF8, "application/json"));
-            res.Wait();
-            var str = res.Result.Content.ReadAsStringAsync();
-            str.Wait();
-            
-            var response = JsonNode.Parse(str.Result);
-
-            if ( response!["message"] != null && response["message"]?.ToString() != "")
-                throw new InvalidOperationException(response["message"]?.ToString());
-        }
-
-        private readonly string _address;
-        private readonly string _id;
-        private readonly string _key;
+        public string alias { get; init; } = "";
+        public string id { get; init; } = "";
+        public string key { get; init; } = "";
     }
+    
+    private struct UnregisterLogRushStreamRequest
+    {
+        public UnregisterLogRushStreamRequest() { }
+        public string id { get; init; } = "";
+        public string key { get; init; } = "";
+    }
+        
+    private struct LogMessageRequest
+    {
+        public LogMessageRequest() { }
+
+        public string stream { get; init; } = "";
+        public string log { get; init; } = "";
+        public long timestamp { get; init; } = 0;
+    }
+
+    public LogRushLogger(string alias, string address, string id = "", string key = "")
+    {
+        using var client = new HttpClient();
+            
+        var json = new RegisterLogRushStreamRequest { alias = alias, id = id, key = key };
+
+        var httpRes = client.PostAsync($"{address}/stream/register", new StringContent(JsonSerializer.Serialize(json), Encoding.UTF8, "application/json")).Result;
+
+        var str = httpRes.Content.ReadAsStringAsync().Result;
+
+        if (!httpRes.IsSuccessStatusCode)
+            throw new InvalidOperationException(str);
+            
+        var response = JsonNode.Parse(str);
+        if ( response!["message"] != null && response["message"]?.ToString() != "")
+            throw new InvalidOperationException(response["message"]?.ToString());
+            
+        _address = address;
+        _id = response["id"]!.ToString();
+        _key = response["key"]!.ToString();
+    }
+        
+    public void Deconstruct()
+    {
+        var json = new UnregisterLogRushStreamRequest { id = _id, key = _key};
+            
+        using var client = new HttpClient();
+        var httpRes = client.PostAsync($"{_address}/stream/unregister", new StringContent(JsonSerializer.Serialize(json), Encoding.UTF8, "application/json"));
+    }
+        
+    public async Task Log(string log)
+    {
+        using var client = new HttpClient();
+
+        var message = new LogMessageRequest
+        {
+            log = log,
+            timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+            stream = _id
+        };
+
+        var res = await client.PostAsync($"{_address}/log", new StringContent(JsonSerializer.Serialize(message), Encoding.UTF8, "application/json"));
+        var str = await res.Content.ReadAsStringAsync();
+            
+        var response = JsonNode.Parse(str);
+
+        if ( response!["message"] != null && response["message"]?.ToString() != "")
+            throw new InvalidOperationException(response["message"]?.ToString());
+    }
+
+    private readonly string _address;
+    private readonly string _id;
+    private readonly string _key;
 }
--- LogRushClient/Log4Net/LogRushLogger.cs
+++ LogRushClient/Log4Net/LogRushLogger.cs
@@ -1,21 +1,23 @@
 using log4net.Appender;
 using log4net.Core;
 
-namespace LogRushClient.Log4Net
+namespace LogRushClient.Log4Net;
+
+public class LogRushLogger : AppenderSkeleton
 {
-    public class LogRushLogger : AppenderSkeleton
+    protected override void Append(LoggingEvent loggingEvent)
     {
-        protected override void Append(LoggingEvent loggingEvent)
+        Task.Run(async () =>
         {
             _logger ??= new Core.LogRushLogger(Alias, Server, Id, Key);
-            _logger.Log(RenderLoggingEvent(loggingEvent));
-        }
-        
-        public string Server { set; get; } = "";
-        public string Alias { set; get; } = "";
-        public string Id { set; get; } = "";
-        public string Key { set; get; } = "";
-        
-        private Core.LogRushLogger? _logger;
+            await _logger.Log(RenderLoggingEvent(loggingEvent));
+        });
     }
+        
+    public string Server { set; get; } = "";
+    public string Alias { set; get; } = "";
+    public string Id { set; get; } = "";
+    public string Key { set; get; } = "";
+        
+    private Core.LogRushLogger? _logger;
 }
